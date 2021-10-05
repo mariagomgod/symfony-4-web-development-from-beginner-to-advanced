8f4711748eee6fbea760a8764e2314d4ade017dd
Maria Gomez <maria_gg86@yahoo.es>
2021-10-05 19:52:12 +0200

    añadidas cookies a DefaultController

--- my_project_name/src/Controller/DefaultController.php
+++ my_project_name/src/Controller/DefaultController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
 use App\Services\GiftsService;
+use Symfony\Component\HttpFoundation\Cookie;
 
 class DefaultController extends AbstractController
 {
@@ -19,9 +20,23 @@ class DefaultController extends AbstractController
 
         $users = $this->getDoctrine()->getRepository(User::class)->findAll();
 
-        $this->addFlash('notice', 'Your changes were saved!');
+        //$this->addFlash('notice', 'Your changes were saved!');
 
-        $this->addFlash('warning', 'Your changes were saved!');
+        //$this->addFlash('warning', 'Your changes were saved!');
+
+        $cookie = new Cookie(
+            'my_cookie',  // Cookie name
+            'cookie value', // Cookie value
+            time() + (2 * 365 * 24 * 60 * 60) // Expires after 2 years
+        );
+
+        //$response = new Response();
+        //$response->headers->setCookie($cookie);
+        //$response->send();
+
+        $response = new Response();
+        $response->headers->clearCookie('my_cookie');
+        $response->send();
 
        //$entityManager->flush(); // flush() save all the users in the database
 
