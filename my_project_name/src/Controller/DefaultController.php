45fd850cbd7ed303ca2d656c81f3acf47ed9ef95
Maria Gomez <maria_gg86@yahoo.es>
2021-10-05 12:53:42 +0200

    creada entidad User y las correspondientes migraciones

--- my_project_name/src/Controller/DefaultController.php
+++ my_project_name/src/Controller/DefaultController.php
@@ -5,13 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/default', name: 'default')]
+    #[Route('/', name: 'default')]
     public function index(): Response
     {
-        $users = ['Adam', 'Robert', 'John', 'Susan'];
+        //$users = [];
+
+        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+      
+        //$entityManager->flush(); // flush() save all the users in the database
+
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
             'users' => $users,
