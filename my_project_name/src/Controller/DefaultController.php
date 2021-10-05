f0d07daa515264b030c7b905dd233cf284c2a8e7
Maria Gomez <maria_gg86@yahoo.es>
2021-10-05 14:02:01 +0200

    creado Service container

--- my_project_name/src/Controller/DefaultController.php
+++ my_project_name/src/Controller/DefaultController.php
@@ -6,21 +6,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
+use App\Services\GiftsService;
 
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'default')]
-    public function index(): Response
+    public function index(GiftsService $gifts): Response
     {
         //$users = [];
 
         $users = $this->getDoctrine()->getRepository(User::class)->findAll();
-      
-        //$entityManager->flush(); // flush() save all the users in the database
+
+       //$entityManager->flush(); // flush() save all the users in the database
 
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
             'users' => $users,
+            'random_gift' => $gifts->gifts,
         ]);
     }
 
